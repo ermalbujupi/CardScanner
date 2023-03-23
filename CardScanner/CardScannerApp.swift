d44756247b2721ab93aaba32451151d47446223a
Ermal Bujupaj <ermal.bj@gmail.com>
2023-03-23 23:25:40 +0100

    first commit

---
+++ CardScanner/CardScannerApp.swift
@@ -0,0 +1,17 @@
+//
+//  CardScannerApp.swift
+//  CardScanner
+//
+//  Created by Ermal Bujupaj on 23.03.23.
+//
+
+import SwiftUI
+
+@main
+struct CardScannerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
