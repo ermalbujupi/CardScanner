906331f85dff68fa2183212f4a96f1896622238f
Ermal Bujupaj <ermal.bj@gmail.com>
2023-03-25 16:16:03 +0100

    Use AppDelegate

--- CardScanner/AppDelegate.swift
+++ CardScanner/AppDelegate.swift
@@ -7,19 +7,23 @@
 
 import UIKit
 
-class SceneDelegate: UIResponder, UIWindowSceneDelegate, UIApplicationDelegate {
+class AppDelegate: UIResponder, UIApplicationDelegate, UIWindowSceneDelegate {
     
     var window: UIWindow?
+
     
+    func application(_ application: UIApplication, configurationForConnecting connectingSceneSession: UISceneSession, options: UIScene.ConnectionOptions) -> UISceneConfiguration {
+        return UISceneConfiguration(name: "Default Configuration", sessionRole: connectingSceneSession.role)
+    }
+
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
         guard let windowScene = (scene as? UIWindowScene) else { return }
         
         let window = UIWindow(windowScene: windowScene)
-        let viewController = UIViewController() //change this
+        let viewController = CardScannerViewController()
         let navigationController = UINavigationController(rootViewController: viewController)
         window.rootViewController = navigationController
         self.window = window
         window.makeKeyAndVisible()
     }
-    
 }
